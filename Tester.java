999b8418acb53e065646b7a2d336f39948426f0c
VincentH-APCS <vhuang10@stuy.edu>
2020-11-02 22:14:46 -0500

    Tested the new add and resizing by going over initial capacity

--- Tester.java
+++ Tester.java
@@ -14,8 +14,30 @@ public class Tester{
    if(Mega.add("Time")){
     System.out.println(Mega.get(3));
    }
-  System.out.println(Mega.set(3, ", there"));
-   System.out.println(Mega.get(3));
+  //System.out.println(Mega.set(3, ", there"));
+   //System.out.println(Mega.get(3));
+   if(Mega.add("There")){
+     System.out.println(Mega.get(4));
+   }
+   if(Mega.add("Was")){
+    System.out.println(Mega.get(5));
+   }
+   if(Mega.add("A")){
+    System.out.println(Mega.get(6));
+   }
+   if(Mega.add("Code")){
+   System.out.println(Mega.get(7));
+   }
+   if(Mega.add("That")){
+     System.out.println(Mega.get(8));
+   }
+  if(Mega.add("Haunted")){
+    System.out.println(Mega.get(9));
+  }
+  //Mega.resize();
+  if(Mega.add("People")){
+    System.out.println(Mega.get(10));
+  }
    System.out.println(Mega.size());
  }
 }
