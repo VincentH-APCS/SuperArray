b846844c8b70ae956ea934db1d517c481aa5e706
VincentH-APCS <vhuang10@stuy.edu>
2020-11-02 21:57:58 -0500

    Simplified Tester, so it utilizes the booleans instead of just returning them

--- Tester.java
+++ Tester.java
@@ -2,17 +2,20 @@
 public class Tester{
  public static void main(String[] args){
    SuperArray Mega = new SuperArray();
-   System.out.println(Mega.add("Once"));
-   System.out.println(Mega.add("Upon"));
-   System.out.println(Mega.add("A"));
-   System.out.println(Mega.add("Time"));
-   System.out.println(Mega.add("takeout-success"));
-   System.out.println(Mega.get(0));
-   System.out.println(Mega.get(1));
-   System.out.println(Mega.get(2));
+    if(Mega.add("Once")){
+      System.out.println(Mega.get(0));
+    }
+   if(Mega.add("Upon")){
+     System.out.println(Mega.get(1));
+   }
+   if(Mega.add("A")){
+     System.out.println(Mega.get(2));
+   }
+   if(Mega.add("Time")){
+    System.out.println(Mega.get(3));
+   }
+  System.out.println(Mega.set(3, ", there"));
    System.out.println(Mega.get(3));
-   System.out.println(Mega.set(4, ", there"));
-   System.out.println(Mega.get(4));
    System.out.println(Mega.size());
  }
 }
