d5e7c82d9e85a3c32edf29e6b2194a3b4d4081f8
VincentH-APCS <vhuang10@stuy.edu>
2020-11-02 21:54:16 -0500

    tested for the set method

--- Tester.java
+++ Tester.java
@@ -5,13 +5,14 @@ public class Tester{
    System.out.println(Mega.add("Once"));
    System.out.println(Mega.add("Upon"));
    System.out.println(Mega.add("A"));
-   System.out.println(Mega.size());
    System.out.println(Mega.add("Time"));
-   System.out.println(Mega.size());
+   System.out.println(Mega.add("takeout-success"));
    System.out.println(Mega.get(0));
    System.out.println(Mega.get(1));
    System.out.println(Mega.get(2));
    System.out.println(Mega.get(3));
-
+   System.out.println(Mega.set(4, ", there"));
+   System.out.println(Mega.get(4));
+   System.out.println(Mega.size());
  }
 }
