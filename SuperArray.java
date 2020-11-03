ce9db756a035e8e6d3bc7eee9074d30ed39a8db5
VincentH-APCS <vhuang10@stuy.edu>
2020-11-02 21:36:52 -0500

    Fixed mistakes in the constructor, ivs, and wrote the method for size()

--- SuperArray.java
+++ SuperArray.java
@@ -2,6 +2,15 @@ public class SuperArray{
   private String[] data;
   private int size;
   public SuperArray(){
-    String[] data = new String[10];
+    data = new String[10];
+  }
+  public int size(){
+    int total = 0;
+    for(int i = 0; i < data.length; i++){
+      if(data[i] != null){
+        total = total + 1;
+      }
+    }
+    return total;
   }
 }
