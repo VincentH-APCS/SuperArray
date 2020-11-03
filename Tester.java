7e22829fbd8b1183704278ee499f252f4a80f21d
VincentH-APCS <vhuang10@stuy.edu>
2020-11-02 21:47:12 -0500

    tested more of add()

--- Tester.java
+++ Tester.java
@@ -3,6 +3,10 @@ public class Tester{
  public static void main(String[] args){
    SuperArray Mega = new SuperArray();
    System.out.println(Mega.add("Once"));
+   System.out.println(Mega.add("Upon"));
+   System.out.println(Mega.add("A"));
+   System.out.println(Mega.size());
+   System.out.println(Mega.add("Time"));
    System.out.println(Mega.size());
  }
 }
