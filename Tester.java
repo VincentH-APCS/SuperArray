33246d53df1f624c3e9e7628acd720a3a10af09b
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 12:23:06 -0500

    performed a test of the isEmpty() method before and after a clear

--- Tester.java
+++ Tester.java
@@ -4,7 +4,9 @@ public class Tester{
   System.out.println(Mega.add("Dragons"));
   System.out.println(Mega.get(0));
   System.out.println(Mega.size());
+  System.out.println(Mega.isEmpty());
   Mega.clear();
   System.out.println(Mega.size());
+  System.out.println(Mega.isEmpty());
 }
 }
