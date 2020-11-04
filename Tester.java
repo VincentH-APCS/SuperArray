938fdbaa30c859e69f80c696100e04232204d2be
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 12:18:10 -0500

    performed a test of the clear method by having size measured before and after the clear

--- Tester.java
+++ Tester.java
@@ -1,72 +1,10 @@
 public class Tester{
  public static void main(String[] args){
    SuperArray Mega = new SuperArray();
-    if(Mega.add("Once")){
-      System.out.println(Mega.get(0));
-    }
-   if(Mega.add("Upon")){
-     System.out.println(Mega.get(1));
-   }
-   if(Mega.add("A")){
-     System.out.println(Mega.get(2));
-   }
-   if(Mega.add("Time")){
-    System.out.println(Mega.get(3));
-   }
-   if(Mega.add("There")){
-     System.out.println(Mega.get(4));
-   }
-   if(Mega.add("Was")){
-    System.out.println(Mega.get(5));
-   }
-   if(Mega.add("A")){
-    System.out.println(Mega.get(6));
-   }
-   if(Mega.add("Code")){
-   System.out.println(Mega.get(7));
-   }
-   if(Mega.add("That")){
-     System.out.println(Mega.get(8));
-   }
-  if(Mega.add("Haunted")){
-    System.out.println(Mega.get(9));
-  }
-  if(Mega.add("People.")){
-    System.out.println(Mega.get(10));
-  }
-  if(Mega.add("Once")){
-    System.out.println(Mega.get(11));
-  }
- if(Mega.add("They")){
-   System.out.println(Mega.get(12));
- }
- if(Mega.add("Type")){
-   System.out.println(Mega.get(13));
- }
- if(Mega.add("It")){
-  System.out.println(Mega.get(14));
- }
- if(Mega.add("Into")){
-   System.out.println(Mega.get(15));
- }
- if(Mega.add("A")){
-  System.out.println(Mega.get(16));
- }
- if(Mega.add("Program")){
-  System.out.println(Mega.get(17));
- }
- if(Mega.add("They")){
- System.out.println(Mega.get(18));
- }
- if(Mega.add("Must")){
-   System.out.println(Mega.get(19));
- }
-if(Mega.add("Dance")){
-  System.out.println(Mega.get(20));
+  System.out.println(Mega.add("Dragons"));
+  System.out.println(Mega.get(0));
+  System.out.println(Mega.size());
+  Mega.clear();
+  System.out.println(Mega.size());
 }
-if(Mega.add("In Their Seat.")){
-  System.out.println(Mega.get(21));
-}
-   System.out.println(Mega.size());
- }
 }
