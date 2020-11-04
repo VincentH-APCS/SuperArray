312942088a3d77fe9b0f87bd146425a654737df1
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 14:20:23 -0500

    The test to see if the new add works

--- Tester.java
+++ Tester.java
@@ -3,13 +3,17 @@ public class Tester{
    SuperArray Mega = new SuperArray(5);
   System.out.println(Mega.add("Dragons"));
   System.out.println(Mega.add("Live"));
-  System.out.println(Mega.add("In the Caves"));
+  System.out.println(Mega.add("Caves"));
   System.out.println(Mega.add("Far From"));
   System.out.println(Mega.add("The Castles."));
   System.out.println(Mega.size());
-  System.out.println(Mega.contains("Live"));
-  System.out.println(Mega.contains("The Castles."));
-  System.out.println(Mega.contains("Far"));
-  System.out.println(Mega.contains("Invisible Man"));
+  System.out.println(Mega.toString());
+  //System.out.println(Mega.contains("Live"));
+  //System.out.println(Mega.contains("The Castles."));
+  //System.out.println(Mega.contains("Far"));
+  //System.out.println(Mega.contains("Invisible Man"));
+  Mega.add(2,"In The");
+  System.out.println(Mega.toString());
+  System.out.println(Mega.size());
 }
 }
