bedd678777a1d1ce77173ca5a454540dab57bdeb
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 14:05:14 -0500

    fixed the mistake in add, where it didn't report the element before the index

--- SuperArray.java
+++ SuperArray.java
@@ -85,7 +85,7 @@ public class SuperArray{
       resize();
     }
     String[] newone = new String[data.length + 1];
-    for(int i = 0; i < index - 1; i++){
+    for(int i = 0; i < index; i++){
       newone[i] = data[i];
     }
     newone[index] = element;
