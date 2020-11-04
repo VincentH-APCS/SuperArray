7810492b07ab3b87dd6323f43450416e8ab8e984
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 12:29:14 -0500

    the test to see if toString works

--- Tester.java
+++ Tester.java
@@ -2,11 +2,11 @@ public class Tester{
  public static void main(String[] args){
    SuperArray Mega = new SuperArray();
   System.out.println(Mega.add("Dragons"));
-  System.out.println(Mega.get(0));
+  System.out.println(Mega.add("Live"));
+  System.out.println(Mega.add("In the Caves"));
+  System.out.println(Mega.add("Far From"));
+  System.out.println(Mega.add("The Castles."));
+  System.out.println(Mega.toString());
   System.out.println(Mega.size());
-  System.out.println(Mega.isEmpty());
-  Mega.clear();
-  System.out.println(Mega.size());
-  System.out.println(Mega.isEmpty());
 }
 }
