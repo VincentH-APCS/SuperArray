a22d7e5052dbeddf6b79f4c373c8c36f1a6ed390
VincentH-APCS <vhuang10@stuy.edu>
2020-11-04 21:36:39 -0500

    Test 3 cases of true, in which one is the very last element, and 3 'false' indexes, one being similar but not a string exactly, and 2 that should return -1

--- Tester.java
+++ Tester.java
@@ -1,24 +1,24 @@
 public class Tester{
  public static void main(String[] args){
    SuperArray Mega = new SuperArray(5);
-  //System.out.println(Mega.add("Dragons"));
-  //System.out.println(Mega.add("Live"));
-  //System.out.println(Mega.add("Caves"));
-  //System.out.println(Mega.add("Far From"));
-  //System.out.println(Mega.add("The Castles."));
+
   Mega.add(0,"Dragons");
   Mega.add(1,"Live");
   Mega.add(2,"In The");
   Mega.add(3, "Caves");
   Mega.add(4, "Far From");
   Mega.add(5, "The Castles.");
-  System.out.println(Mega.size());
-  System.out.println(Mega.toString());
-  System.out.println(Mega.add("Invisble Ghost"));
-  System.out.println(Mega.add("Magic--disappear"));
+  Mega.add(6, "Invisble Ghost"));
+  Mega.add(7, "Magic--disappear"));
   System.out.println(Mega.size());
   System.out.println(Mega.remove(7));
   System.out.println(Mega.toString());
   System.out.println(Mega.size());
+  System.out.println(Mega.indexOf("In The"));
+  System.out.println(Mega.indexOf("Invisible Ghost"));
+  System.out.println(Mega.indexOf("Far From"));
+  System.out.println(Mega.indexOf("Far"));
+  System.out.println(Mega.indexOf("Carmen Sandiego"));
+  System.out.println(Mega.indexOf("negative 1"));
 }
 }
