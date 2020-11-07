95e9f41376ee84079a345127c4d1f24bfa173e66
VincentH-APCS <vhuang10@stuy.edu>
2020-11-07 01:43:21 -0500

    tested the lastIndexOf

--- Demo.java
+++ Demo.java
@@ -1,11 +1,11 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
-for(int i = s.size(); i > 0; i--){
-  if(s.indexOf(s.get(i)) < i){
-    s.remove(i);
+   for(int i = s.size(); i > 0; i--){
+      if(s.indexOf(s.get(i)) < i){
+         s.remove(i);
+      }
+    }
   }
- }
-}
   public static SuperArray findOverlap(SuperArray a, SuperArray b){
     SuperArray likes = new SuperArray();
     for(int i = 0; i < a.size(); i++){
@@ -20,6 +20,7 @@ for(int i = s.size(); i > 0; i--){
     }
     return likes;
   }
+
   public static void main(String[] args){
     SuperArray words = new SuperArray();
     words.add("kani");  words.add("uni"); words.add("ebi");
@@ -29,11 +30,13 @@ for(int i = s.size(); i > 0; i--){
     words.add("ebi"); words.add("toro");
     words.add("ebi"); words.add("toro");*/
     SuperArray letters = new SuperArray();
-    letters.add("uni"); letters.add("dino"); letters.add("four");
+    letters.add("dino"); letters.add("four"); letters.add("uni");
     letters.add("kani"); letters.add("uni"); letters.add("test");
 
     System.out.println(words);
     System.out.println(letters);
-    System.out.println(findOverlap(letters, words));
+    System.out.println(words.lastIndexOf("numb"));
+    System.out.println(words.lastIndexOf("ebi"));
+    System.out.println(letters.lastIndexOf("dino"));
   }
 }
