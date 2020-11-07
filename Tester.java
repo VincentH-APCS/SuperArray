68c472ca9a5c4ef8966d2b1284ff9f1c7d3dafa7
VincentH-APCS <vhuang10@stuy.edu>
2020-11-07 01:04:06 -0500

    It seems it's the end of tester, and now it's only demo

--- Tester.java
+++ Tester.java
@@ -1,26 +1,20 @@
 public class Tester{
  public static void main(String[] args){
-   SuperArray Mega = new SuperArray(5);
+   SuperArray Mega = new SuperArray();
 
   Mega.add(0,"Dragons");
-  Mega.add(1,"Live");
-  Mega.add(2,"In The");
-  Mega.add(3, "Caves");
-  Mega.add(4, "Magic--disappear");
-  Mega.add(5, "Far From");
-  Mega.add(6, "Invisible Ghost");
-  Mega.add(6, "The Castles.");
-  System.out.println(Mega.size());
-  System.out.println(Mega.remove(4));
+  Mega.add(1,"danger");
+  Mega.add(2, "The");
+  Mega.add(3, "Magic");
+  Mega.add(4, "random");
+  Mega.add(5, "The");
+  Mega.add(6, "danger");
+  Mega.add(7, "danger");
+  Mega.add(8, "danger");
   System.out.println(Mega.toString());
   System.out.println(Mega.size());
-  System.out.println(Mega.indexOf("In The"));
-  System.out.println(Mega.indexOf("Invisible Ghost"));
-  System.out.println(Mega.indexOf("Far From"));
-  System.out.println(Mega.indexOf("Far"));
-  System.out.println(Mega.indexOf("Carmen Sandiego"));
-  System.out.println(Mega.indexOf("negative 1"));
-  System.out.println(Mega.toArray());
-  System.out.println(Mega.getData());
+  //SuperArray.removeDuplicates(Mega);
+  System.out.println(Mega.remove(2));
+  System.out.println(Mega.toString());
 }
 }
