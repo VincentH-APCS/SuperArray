6807ae72c9a42cd5afde509ec4abe4145de17021
VincentH-APCS <vhuang10@stuy.edu>
2020-11-07 01:03:18 -0500

    Finally got removeDuplicates to work-- massive thanks to Reng SZheng for helping me out

--- Demo.java
+++ Demo.java
@@ -1,15 +1,11 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
-    for(int i = 0; i < s.size();){
-      if(s.indexOf(s.get(i)) != i){
-        s.remove(i);
-        i = i;
-      }
-      else{
-      i++;
-      }
-    }
+for(int i = s.size(); i > 0; i--){
+  if(s.indexOf(s.get(i)) < i){
+    s.remove(i);
   }
+ }
+}
   public static void main(String[] args){
     SuperArray words = new SuperArray();
     words.add("kani");  words.add("uni"); words.add("ebi");
