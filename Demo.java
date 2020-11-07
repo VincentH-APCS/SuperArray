3a57f94e2e89f870192d56b5f9e38a09a2dc46ad
VincentH-APCS <vhuang10@stuy.edu>
2020-11-07 01:28:13 -0500

    Adding the FindOverlap method

--- Demo.java
+++ Demo.java
@@ -6,17 +6,33 @@ for(int i = s.size(); i > 0; i--){
   }
  }
 }
+  public static SuperArray findOverlap(SuperArray a, SuperArray b){
+    SuperArray likes = new SuperArray();
+    for(int i = 0; i < a.size(); i++){
+      if(likes.contains(a.get(i))){
+        likes = likes;
+      }
+      else{
+        if(b.contains(a.get(i))){
+          likes.add(a.get(i));
+        }
+      }
+    }
+    return likes;
+  }
   public static void main(String[] args){
     SuperArray words = new SuperArray();
     words.add("kani");  words.add("uni"); words.add("ebi");
     words.add("una"); words.add("una"); words.add("ebi");
-    words.add("kani"); words.add("una"); words.add("una");
+    /*words.add("kani"); words.add("una"); words.add("una");
     words.add("ebi"); words.add("toro");
+    words.add("ebi"); words.add("toro");*/
+    SuperArray letters = new SuperArray();
+    letters.add("uni"); letters.add("dino"); letters.add("four");
+    letters.add("kani");
 
-    System.out.println(words.size());
     System.out.println(words);
-    //System.out.println(words.remove(3));
-    removeDuplicates(words);
-    System.out.println(words);
+    System.out.println(letters);
+    System.out.println(findOverlap(words, letters));
   }
 }
