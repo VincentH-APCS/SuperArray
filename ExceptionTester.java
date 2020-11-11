72c9ae9f300a573282050a8b6be882338a32c793
VincentH-APCS <vhuang10@stuy.edu>
2020-11-10 23:45:56 -0500

    Testing the cases for add with exceptions

--- ExceptionTester.java
+++ ExceptionTester.java
@@ -7,36 +7,37 @@ public class ExceptionTester{
        System.out.println("We've done a crash!");
      }
   SuperArray Mega = new SuperArray();
-     Mega.add(0,"Dragons");
-     Mega.add(1,"danger");
-     Mega.add(2, "The");
-     Mega.add(3, "Magic");
-     Mega.add(4, "random");
-  System.out.println(Mega.size());
+     System.out.println(Mega.add("Dragons"));
+     System.out.println(Mega.add("danger"));
+     System.out.println(Mega.add("The"));
+     System.out.println(Mega.add("Magic"));
+     System.out.println(Mega.add("random"));
+    System.out.println(Mega.size());
 
   try{
-    Mega.set(-5, "error");
+    Mega.add(-5, "error");
   } catch(
     IndexOutOfBoundsException e){
       System.out.println("No Negatives!");
     }
     try{
-      Mega.set(555, "elephant");
+      Mega.add(555, "elephant");
     } catch(
       IndexOutOfBoundsException e){
         System.out.println("TOO BIG!");
       }
       try{
-        Mega.set(5, "samesies");
+        Mega.add(5, "samesies");
       } catch(
         IndexOutOfBoundsException e){
           System.out.println("There can only be one");
         }
      try{
-          Mega.set(4, "No Problemo?");
+          Mega.add(4, "No Problemo?");
         } catch(
           IndexOutOfBoundsException e){
             System.out.println("Oh no");
           }
+      System.out.println(Mega);
    }
   }
