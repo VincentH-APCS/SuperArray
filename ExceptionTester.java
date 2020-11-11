011e9f8f243bc7b689cc240a8fcaad5d6c548bc2
VincentH-APCS <vhuang10@stuy.edu>
2020-11-10 23:19:48 -0500

    tested the exceptions of get

--- ExceptionTester.java
+++ ExceptionTester.java
@@ -6,11 +6,32 @@ public class ExceptionTester{
      IllegalArgumentException e){
        System.out.println("We've done a crash!");
      }
-   try{
-     SuperArray NoCrasher = new SuperArray(5);
-   } catch(
-     IllegalArgumentException e){
-       System.out.println("Oh no check again");
-     }
+  SuperArray Mega = new SuperArray();
+     Mega.add(0,"Dragons");
+     Mega.add(1,"danger");
+     Mega.add(2, "The");
+     Mega.add(3, "Magic");
+     Mega.add(4, "random");
+  System.out.println(Mega.size());
+
+  try{
+    Mega.get(-5);
+  } catch(
+    IndexOutOfBoundsException e){
+      System.out.println("No Negatives!");
+    }
+    try{
+      Mega.get(555);
+    } catch(
+      IndexOutOfBoundsException e){
+        System.out.println("TOO BIG!");
+      }
+      try{
+        Mega.get(2);
+      } catch(
+        IndexOutOfBoundsException e){
+          System.out.println("Check code buddy");
+        }
+    System.out.println(Mega.get(2));
    }
   }
